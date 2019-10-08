7a98aa22d01a139ffa73f18891ab95ab3880be11
genkyo@163.com <5517546kyo>
2019-10-08 10:22:40 +0800

    注释有误修改

--- richinfo-biz-attendance/rh-attendance/src/main/java/richinfo/attendance/entity/UserInfo.java
+++ richinfo-biz-attendance/rh-attendance/src/main/java/richinfo/attendance/entity/UserInfo.java
@@ -99,7 +99,7 @@ public class UserInfo implements Serializable
     private Integer smsSwitch;
 
     /**
-     * 首次发送信息表示 空位第一次 1：非第一次发送
+     * 首次发送信息标识 0:非第一次发送 1：第一次发送
      * */
     private Integer firstSend;
 
